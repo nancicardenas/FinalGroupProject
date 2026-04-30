dc01275ab5cba3752231b80c1b1f88bea592c88a
dylanrambo26 <dylan.rambo26@gmail.com>
2026-04-29 18:43:41 -0700

    Added human field of view detection

--- FinalGame/Assets/Scripts/State Machine/HumanAI.cs
+++ FinalGame/Assets/Scripts/State Machine/HumanAI.cs
@@ -1,5 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class HumanAI : MonoBehaviour
 {
@@ -22,6 +25,34 @@ public class HumanAI : MonoBehaviour
     private float humanYPosition = 0.6f;
 
     private Vector3 destinationPos;
+
+    public Transform target;
+    
+    //Change These when resizing cat/human objects
+    private float humanHeight = 2.3f;
+    private float catHeight = 0.5f;
+    private float ghostHeight = 0.3f;
+    private float targetHeight;
+
+    private float viewDistance = 15f;
+    private float viewAngle = 100f;
+    private float catchRadius = 1f;
+    
+    public bool isTargetPlayer = true;
+
+    //TODO: delete later (for testing in AI scene)
+    private CameraFollow cameraFollow;
+    
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        //TODO: delete later (for testing in AI scene)
+        if (SceneManager.GetActiveScene().name == "Human AI Test")
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>().target = target.transform;
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -65,6 +96,7 @@ public class HumanAI : MonoBehaviour
     private void EnterWalking()
     {
         humanAgent.isStopped = false;
+        humanAgent.speed = 2f;
         destinationPos = new Vector3(Random.Range(-9, 9), humanYPosition, Random.Range(-9, 9));
         humanAgent.SetDestination(destinationPos);
         state = humanState.walking;
@@ -79,6 +111,10 @@ public class HumanAI : MonoBehaviour
 
     private void UpdateWalking()
     {
+        if (CanSeePlayer())
+        {
+            EnterAlert();
+        }
         if (HasReachedDestination())
         {
             EnterIdle();
@@ -87,21 +123,127 @@ public class HumanAI : MonoBehaviour
     
     private void EnterAlert()
     {
-        
+        humanAgent.isStopped = false;
+        humanAgent.speed = 6f;
+        state = humanState.alert;
     }
 
     private void UpdateAlert()
     {
-        
+        if (CanSeePlayer())
+        {
+            humanAgent.SetDestination(target.position);
+            return;
+        }
+
+        if (ReachedTarget())
+        {
+            EnterPlayerCaught();
+            return;
+        }
+        humanAgent.ResetPath();
+        EnterIdle();
     }
-    
+
+    bool ReachedTarget()
+    {
+        return Vector3.Distance(target.position, transform.position) <= catchRadius;
+    }
     private void EnterPlayerCaught()
     {
-        
+        print("playerCaught");
+        state = humanState.playerCaught;
     }
 
     private void UpdatePlayerCaught()
     {
+        print("done");
+    }
+    
+    bool CanSeePlayer()
+    {
+        targetHeight = isTargetPlayer ? catHeight : ghostHeight;
         
+        //Ray setup
+        Vector3 origin = transform.position + Vector3.up * humanHeight;
+        Vector3 targetPosition = target.position + Vector3.up * targetHeight;
+        Vector3 dir = (targetPosition - origin).normalized;
+        
+        float dist = Vector3.Distance(origin, targetPosition);
+
+        //The distance between the player and the human has to be less than the viewDistance to return true
+        if (dist > viewDistance) 
+            return false;
+
+        float angleToTarget = Vector3.Angle(transform.forward, dir);
+        if (angleToTarget > viewAngle * 0.5f) 
+            return false;
+
+        //Cast a ray toward the player to see if there is an obstacle in the way
+        if (Physics.Raycast(origin, dir, out RaycastHit hit, dist))
+        {
+            return hit.transform == target;
+        }
+
+        return false;
+    }
+    
+    //Helper used to draw gizmos in the editor for detection
+    void OnDrawGizmosSelected()
+    {
+        if (this.target == null) return;
+
+        Vector3 origin = transform.position + Vector3.up * humanHeight;
+
+        // --- Draw detection radius ---
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(origin, viewDistance);
+
+        // --- Draw FOV cone ---
+        Gizmos.color = Color.blue;
+
+        int segments = 30; // More = smoother arc
+        float halfFOV = viewAngle * 0.5f;
+
+        Vector3 previousPoint = origin;
+
+        for (int i = 0; i <= segments; i++)
+        {
+            float angle = -halfFOV + (viewAngle / segments) * i;
+            Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+            Vector3 point = origin + direction * viewDistance;
+
+            // Draw lines forming the arc
+            if (i > 0)
+            {
+                Gizmos.DrawLine(previousPoint, point);
+            }
+
+            // Draw lines from origin to edge (cone sides)
+            if (i == 0 || i == segments)
+            {
+                Gizmos.DrawLine(origin, point);
+            }
+
+            previousPoint = point;
+        }
+
+        // --- Existing player line + raycast ---
+        Vector3 targetPos = this.target.position + Vector3.up * targetHeight;
+        Vector3 dir = (targetPos - origin).normalized;
+        float dist = Vector3.Distance(origin, targetPos);
+
+        Gizmos.color = Color.gray;
+        Gizmos.DrawLine(origin, targetPos);
+
+        if (dist <= viewDistance)
+        {
+            if (Physics.Raycast(origin, dir, out RaycastHit hit, dist))
+            {
+                Gizmos.color = CanSeePlayer() ? Color.green : Color.red;
+                Gizmos.DrawLine(origin, hit.point);
+                Gizmos.DrawSphere(hit.point, 0.1f);
+            }
+        }
     }
 }
